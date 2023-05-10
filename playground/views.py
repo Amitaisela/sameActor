693e8ccd96004506a68c7fe27860bde22fc7f25b
Amitaisela <68296620+Amitaisela@users.noreply.github.com>
2023-05-10 20:13:41 +0300

    added notes

--- playground/views.py
+++ playground/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 import config
+
 # Create your views here.
 api_key = config.API_KEY
 base_url = 'https://api.themoviedb.org/3'
@@ -20,22 +21,29 @@ def getActors(request):
 def get_common_actors(request, movie_titles, htmlSite):
     ia = imdb.IMDb()
     movie_ids = []
+    # find movies based on given title
     for title in movie_titles:
         search_results = ia.search_movie(title)
         if search_results:
             movie_id = search_results[0].getID()
             movie_ids.append(movie_id)
     common_actors = set()
+    # if we have more than one movie. ( redundant )
     if len(movie_ids) > 0:
+        # make sure that we have at least one actor in the set ( redundant )
         first_movie = ia.get_movie(movie_ids[0])
         common_actors.update([actor['name'] for actor in first_movie['cast']])
+
+        # addes all the other actors from the other movies
         for movie_id in movie_ids[1:]:
             movie = ia.get_movie(movie_id)
             common_actors.intersection_update(
                 [actor['name'] for actor in movie['cast']])
-    results = list(common_actors)
 
+    results = list(common_actors)
     actorsDict = {}
+
+    # find the rest of the information on the actors from the results list
     for result in results:
         search_url = f'{base_url}/search/person'
         params = {
@@ -45,6 +53,8 @@ def get_common_actors(request, movie_titles, htmlSite):
 
         response = requests.get(search_url, params=params)
         results = response.json()['results']
+
+        # if for some reason the actor didnt have any info, continue to the next one
         if len(results) == 0:
             continue
         actor = results[0]
@@ -53,7 +63,7 @@ def get_common_actors(request, movie_titles, htmlSite):
         response = requests.get(details_url, params=params)
         actor_details = response.json()
 
-        # actorsDict[actor_details['name']] = [actor_details['name'], actor_details['birthday'],f'https://image.tmdb.org/t/p/w500{actor_details["profile_path"]}']
+        # find age
         if actor_details['birthday'] != None:
             date_format = '%Y-%m-%d'
             birth_date = datetime.datetime.strptime(
